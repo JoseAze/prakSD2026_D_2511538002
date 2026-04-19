c01cb63ad0def4f8388f99da8d18c7421a54b134
JoseAze <josemonizdearouju@gmail.com>
2026-04-19 23:17:41 +0700

    Add files via upload

---
+++ prakSD2026_D_2511538002/src/pekan2_2511538002/laporan pekan 2 dan tugas/Musik_2511538002.java
@@ -0,0 +1,30 @@
+package pekan2_2511538002;
+
+public class Musik_2511538002 { 
+	private String Judul_8002;
+	private String Penyanyi_8002;
+	private int Durasi_8002;
+	
+	//Konstruktor
+	public Musik_2511538002(String Judul, String Penyanyi, int Durasi) {
+		this.Judul_8002=Judul;
+		this.Penyanyi_8002=Penyanyi;
+		this.Durasi_8002=Durasi;
+		
+	}
+	//Getter
+	public String getJudul_8002() { return Judul_8002;}
+	public String getPenyanyi_8002() { return Penyanyi_8002;}
+	public int getDurasi_8002() { return Durasi_8002;}
+	
+	//Setter 
+	public void setJudul_8002(String Judul) { this.Judul_8002=Judul;}
+	public void setPenyanyi_8002(String Penyanyi) { this.Penyanyi_8002=Penyanyi;}
+	public void setDurasi_8002(int Durasi) { this.Durasi_8002=Durasi;}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
